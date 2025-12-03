d1db2442443297938a38cd782d6315e5a9d3e2e9
EmilyWatkinz <sewatkins16@gmail.com>
2025-12-02 18:31:08 -0800

    Responsive Shapes

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-//template_iavynmu
-//service_5bpn8ak
-//wi6ra8rOTt_-ijw2w
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1/20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+      shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+     }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle)  {
+        document.body.classList += " dark__theme"
+    } 
+    else {
+        document.body.classList.remove("dark__theme");
+    }
+}
+
 
 function contact() {
     event.preventDefault();
@@ -24,7 +47,6 @@ function contact() {
     })
 }
 
-let isModalOpen = false;
 function toggleModal() {
     if (isModalOpen) {
         isModalOpen = false;
